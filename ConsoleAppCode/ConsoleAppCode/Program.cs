a95363e2682f4a7d4ce3024c7141c2578ce3ada8
hritammondal <hritammondal@outlook.com>
2021-08-23 12:10:33 +0530

    Version 2.0

--- ConsoleAppCode/ConsoleAppCode/Program.cs
+++ ConsoleAppCode/ConsoleAppCode/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
+            Console.WriteLine("Welcome to Mindteck!");
         }
     }
 }
